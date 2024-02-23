399885cebfd2b40769eace9fc6bad03ce2a82423
Samu Cabrera <samucabreram99@gmail.com>
2024-02-23 15:51:57 -0300

    Pipes numéricos y no comunes añadidos

--- src/app/products/number-page/number-page.component.ts
+++ src/app/products/number-page/number-page.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './number-page.component.css'
 })
 export class NumberPageComponent {
-
+  public totalSells: number = 25677789.5565;
+  public percent: number = 0.4888;
 }
--- src/app/products/oncommon-page/oncommon-page.component.ts
+++ src/app/products/oncommon-page/oncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-oncommon-page',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './oncommon-page.component.css'
 })
 export class OncommonPageComponent {
+  //i18nSelect para cambiar palabras 
+  public name: string = 'samuel';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
 
+  changeName(): void {
+    this.name = 'Fernanda';
+    this.gender = 'female';
+  }
+
+
+  //pip i18nPlural
+  public clients: string[] = ['andres', 'david', 'fer', 'marcos'];
+  public clientMap = {
+    '=0': 'No tenemos clientes esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  //keyvalue
+  public person = {
+    name: 'Samu',
+    apellido: 'Cabrera'
+  }
+
+  //async pipe
+  public observableTimer: Observable<number> = interval(2000).pipe(
+    tap(value => console.log(value))
+  )
 }
